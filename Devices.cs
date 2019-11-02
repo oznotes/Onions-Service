42ac6609d9646fc3558707148dbd731fee559eaa
Oz <oz.ozgur@ymail.com>
2019-11-02 23:55:06 +0800

    moved to control

--- Devices.cs
+++ Devices.cs
@@ -21,48 +21,4 @@ namespace Onions
         public List<MODELIST> MODELIST { get; set; }
     }
 
-    public static class MainFormVisible
-    {
-        public static bool IsVisible { get; set; }
-    }
-
-    public static class SearchDialogVisible 
-    {
-        private static bool IsAvailable;
-
-        public static bool IsThisVisible
-        {
-            
-            get{return IsAvailable; }
-            set
-            {
-                IsAvailable = value;
-                MainForm.SearchDialogVisibleChanged();
-            }
-        }
-
-    }
-
-    public static class WhatLanguageIsActivate
-    {
-        public static string ThisLanguage
-        {
-            get 
-            {
-                string[] lines = File.ReadAllLines(@"config.ini");
-                return lines[0].ToString();
-            }
-            set
-            {
-                using (StreamWriter writetext = new StreamWriter(@"config.ini"))
-                {
-                    writetext.WriteLine(value);
-                }
-
-            }
-
-        }
-            
-
-    }
 }
---
+++ Kontrol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onions
+{
+    public static class Kontrol
+    {
+        public static class MainFormVisible
+        {
+            public static bool IsVisible { get; set; }
+        }
+
+        public static class SearchDialogVisible
+        {
+            private static bool IsAvailable;
+
+            public static bool IsThisVisible
+            {
+
+                get { return IsAvailable; }
+                set
+                {
+                    IsAvailable = value;
+                    MainForm.SearchDialogVisibleChanged();
+                }
+            }
+
+        }
+
+        public static class WhatLanguageIsActivate
+        {
+            public static string ThisLanguage
+            {
+                get
+                {
+                    string[] lines = File.ReadAllLines(@"config.ini");
+                    return lines[0].ToString();
+                }
+                set
+                {
+                    using (StreamWriter writetext = new StreamWriter(@"config.ini"))
+                    {
+                        writetext.WriteLine(value);
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
