1de382890ce4a8893075bfa30cdafea533bc28da
Oz <oz.ozgur@ymail.com>
2019-11-03 00:03:05 +0800

    Update Kontrol.cs

--- Kontrol.cs
+++ Kontrol.cs
@@ -7,51 +7,41 @@ using System.Threading.Tasks;
 
 namespace Onions
 {
-    public static class Kontrol
+    public static class MainFormVisible
     {
-        public static class MainFormVisible
-        {
-            public static bool IsVisible { get; set; }
-        }
+        public static bool IsVisible { get; set; }
+    }
 
-        public static class SearchDialogVisible
+    public static class SearchDialogVisible
+    {
+        private static bool IsAvailable;
+        public static bool IsThisVisible
         {
-            private static bool IsAvailable;
-
-            public static bool IsThisVisible
+            get { return IsAvailable; }
+            set
             {
-
-                get { return IsAvailable; }
-                set
+                IsAvailable = value;
+                MainForm.SearchDialogVisibleChanged();
+            }
+        }
+    }
+    public static class WhatLanguageIsActivate
+    {
+        public static string ThisLanguage
+        {
+            get
+            {
+                string[] lines = File.ReadAllLines(@"config.ini");
+                return lines[0].ToString();
+            }
+            set
+            {
+                using (StreamWriter writetext = new StreamWriter(@"config.ini"))
                 {
-                    IsAvailable = value;
-                    MainForm.SearchDialogVisibleChanged();
+                    writetext.WriteLine(value);
                 }
             }
-
-        }
-
-        public static class WhatLanguageIsActivate
-        {
-            public static string ThisLanguage
-            {
-                get
-                {
-                    string[] lines = File.ReadAllLines(@"config.ini");
-                    return lines[0].ToString();
-                }
-                set
-                {
-                    using (StreamWriter writetext = new StreamWriter(@"config.ini"))
-                    {
-                        writetext.WriteLine(value);
-                    }
-
-                }
-
-            }
-
-
         }
     }
 }
+
