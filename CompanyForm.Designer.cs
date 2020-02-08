95c379ac84c17a5623ad589d3afa1658185c7602
Oz <oz.ozgur@ymail.com>
2020-02-08 14:06:52 +0800

    Fixed.Single

--- CompanyForm.Designer.cs
+++ CompanyForm.Designer.cs
@@ -176,6 +176,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cmpName);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "CompanyForm";
             this.ShowIcon = false;
